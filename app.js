3888a31ace03bfafe9275833a0ca0022f848cf07
csm <csm009177@gmail.com>
2023-10-26 16:23:11 +0900

    serv2 is about make serv with 5 request

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-const http = require('http');
-const fs = require('fs');
-let port = 2317
+const express = require('express');
+const app = express();
+const port = 3000;
 
-let serv = http.createServer((req, res) => {
-  // 
-  if(req.method === 'GET' && req.url === '/'){
-    
+// 정적 파일 서빙을 설정합니다.
+app.use(express.static('public'));
+
+// Body 파서 설정 (POST 요청의 body를 파싱하기 위해)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET 요청으로 index.html 파일을 읽어옵니다.
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// POST 요청으로 로그인 데이터를 받습니다.
+app.post('/login', (req, res) => {
+  // 이곳에서 POST 데이터를 처리할 수 있습니다.
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // 로그인 처리 코드를 작성하세요.
+
+  // 예시 응답
+  if (username === 'user' && password === 'password') {
+    res.send('로그인 성공');
+  } else {
+    res.send('로그인 실패');
   }
-})
+});
 
-serv.listen(port, ()=>{
-  console.log(`
-please click with under url
-http://localhost:${port}
-`)
-})
+// GET 요청으로 gameOne.html 파일을 읽어옵니다.
+app.get('/gameOne', (req, res) => {
+  res.sendFile(__dirname + '/public/gameOne.html');
+});
+
+// GET 요청으로 gameTwo.html 파일을 읽어옵니다.
+app.get('/gameTwo', (req, res) => {
+  res.sendFile(__dirname + '/public/gameTwo.html');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+});
