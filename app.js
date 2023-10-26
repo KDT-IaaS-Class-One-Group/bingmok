c5dde83003053072887a408aacb9708a231c9497
csm <csm009177@gmail.com>
2023-10-26 16:01:23 +0900

    make serv bone

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const http = require('http');
+const fs = require('fs');
+let port = 2317
+
+let serv = http.createServer((req, res) => {
+  if(req.method === 'GET' && req.url === '/'){
+    
+  }
+})
+
+serv.listen(port, ()=>{
+  console.log(`
+please click with under url
+http://localhost:${port}
+`)
+})
