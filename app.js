a5f4efe7c7b12f21c1fc2c186314961ccdcfdc3b
csm <csm009177@gmail.com>
2023-11-02 09:37:50 +0900

    link with mysql

--- app.js
+++ app.js
@@ -2,58 +2,56 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// 정적 파일 서빙을 설정합니다.
-app.use(express.static('static'));
+// MySQL 모듈을 불러옵니다.
+const mysql = require('mysql');
+
+// MySQL 연결 정보를 설정합니다.
+const db = mysql.createConnection({
+  host: 'your-mysql-host',
+  user: 'your-mysql-username',
+  password: 'your-mysql-password',
+  database: 'your-database-name'
+});
+
+// MySQL 연결을 시작합니다.
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결에 실패했습니다: ' + err.stack);
+    return;
+  }
+  console.log('MySQL 연결이 스레드 ID ' + db.threadId + '로 성공적으로 연결되었습니다.');
+});
 
 // Body 파서 설정 (POST 요청의 body를 파싱하기 위해)
-// urlencoded : 서버가 클라이언트로부터 URL-encoded 데이터를 받을 때 이 데이터를 파싱하고 JavaScript 객체로 변환
-// urlencoded 형식은 웹 양식에서 보낸 데이터를 서버에서 쉽게 읽을 수 있는 형식으로 인코딩하는 방식
-// urlencoded 형식의 요청 데이터를 파싱하기 위한 미들웨어를 설정 
-// extended : false => parse to simple object with QueryString library
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
-// GET 요청으로 index.html 파일을 읽어옵니다.
 app.get('/', (req, res) => {
-  //sendFile로 ( 현재directory + 파일 directory ) 응답한다
-  // __dirname : 읽어올 파일의 디렉토리 + 경로
-    res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 app.post('/', (req, res) => {
-  // 클라이언트에서 제출한 사용자 이름과 암호를 가져온다
   const submittedUsername = req.body.username;
   const submittedPassword = req.body.password;
-  // 실제 사용자 이름과 암호를 설정. 이 예제에서는 하드코딩하여 비교
   const realUsername = 'bingmok';
   const realPassword = '1234';
-  // 사용자 이름과 암호를 확인하고 로그인 결과를 반환
-  if (submittedUsername === realUsername && submittedPassword === realPassword) {
-    // 로그인 성공 시
-    res.sendFile(__dirname + '/static/afterlogin.html');
-    // GET 요청으로 odhello.html 파일을 읽어옴
-    app.get('/static/odhello.html', (req, res) => {
-      res.sendFile(__dirname + '/static/odhello.html');
-      app.get('/static/oshelloScript.js', (req, res) => {
-        res.sendFile(__dirname + '/static/oshelloScript.js');
-      });
-    });
 
-    // GET 요청으로 bingo.html 파일을 읽어옴
-    app.get('/static/bingo.html', (req, res) => {
-      res.sendFile(__dirname + '/static/bingo.html');
+  if (submittedUsername === realUsername && submittedPassword === realPassword) {
+    // 로그인 성공 시, MySQL 데이터를 가져오기
+    db.query('SELECT * FROM your_table_name', (error, results, fields) => {
+      if (error) {
+        console.error('데이터를 가져오는 중 오류 발생: ' + error);
+        res.send('데이터를 가져오는 중 오류가 발생했습니다.');
+      } else {
+        // 결과를 클라이언트에 응답으로 보냅니다.
+        res.json(results);
+      }
     });
   } else {
-    // 로그인 실패 시
-    // send() : express method / send http res to client
     res.send('로그인 실패');
   }
 });
-// serv.listen
+
 app.listen(port, () => {
-  console.log(`
-서버가 포트 ${port}에서 실행 중입니다.
-http://localhost:${port}
-`);
+  console.log(`서버가 포트 ${port}에서 실행 중입니다. http://localhost:${port}`);
 });
